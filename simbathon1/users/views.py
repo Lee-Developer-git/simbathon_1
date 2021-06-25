59fb6a4080a00b51b440112c494ba4149dec395b
Seulgi_LEE <dl4459@naver.com>
2021-06-25 23:07:23 +0900

    update mypage / update new_page

--- simbathon1/users/views.py
+++ simbathon1/users/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from main.models import Blog, Vlog
 from django.contrib.auth.models import User
+from .models import Profile
 
 # Create your views here.
 def mypage(request):
     user = request.user
     blogs = Blog.objects.filter(writer=user)
     vlogs = Vlog.objects.filter(writer=user) 
-    return render(request, 'users/mypage.html', {'blogs': blogs, 'vlogs': vlogs})
+    profile = Profile.objects.filter(user=user)
+    return render(request, 'users/mypage.html', {'blogs': blogs, 'vlogs': vlogs, 'profile': profile})
